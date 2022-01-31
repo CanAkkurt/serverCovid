07161d361284f00a2cd6ee746f80217818193ac5
KADIR AKKURT <kadir.akkurt@student.hogent.be>
2022-01-31 17:19:00 +0100

    Update production.js

--- config/production.js
+++ config/production.js
@@ -4,7 +4,31 @@ module.exports = {
     disabled: false,
   },
   cors: {
-    origins : ['http://localhost:3000'],
-    maxAge: 3 * 60 * 60,
-  }
+    origins: ['http://localhost:3000'],
+    maxAge: 3 * 60 * 60, // 3h in seconds
+  },
+  database: {
+    client: 'mysql2',
+    host: 'localhost',
+    port: 3306,
+    name: 'budget',
+  },
+  pagination: {
+    limit: 100,
+    offset: 0,
+  },
+  auth: {
+    argon: {
+      saltLength: 16,
+      hashLength: 32,
+      timeCost: 6,
+      memoryCost: 2 ** 17,
+    },
+    jwt: {
+      // secret comes via env
+      expirationInterval: 3 * 24 * 60 * 60 * 1000, // ms (3 days)
+      issuer: 'budget.hogent.be',
+      audience: 'budget.hogent.be',
+    },
+  },
 };
